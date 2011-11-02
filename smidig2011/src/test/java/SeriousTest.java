a837304de264f2517fcb9b4cbcc6d3f44b05de4a
Henrik Brautaset Aronsen <hba@conduct.no>
2011-11-02 20:36:42 +0100

    Added database integration test

--- smidig2011/src/test/java/SeriousTest.java
+++ smidig2011/src/test/java/SeriousTest.java
@@ -1,9 +1,34 @@
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import static org.junit.Assert.assertEquals;
 
 public class SeriousTest {
 
+    Connection conn;
+
+    @Before
+    public void setup() throws Exception {
+        Class.forName("org.sqlite.JDBC");
+        conn = DriverManager.getConnection("jdbc:sqlite:/opt/sqlite/smidig2011.sqlite");
+    }
+
     @Test
-    public void shouldNotFail() {
-        assert true;
+    public void shouldNotFail() throws Exception {
+        Statement stat = conn.createStatement();
+        ResultSet rs = stat.executeQuery("select message from smidig2011;");
+        assertEquals("woop woop", rs.getString("message"));
+        rs.close();
+    }
+
+    @After
+    public void teardown() throws Exception {
+        conn.close();
     }
 }
